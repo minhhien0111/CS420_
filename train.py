bc42e7742b5e1228fef802fc0a183fde8ea21738
minhhien0111 <21520229@gm.uit.edu.vn>
2024-12-06 05:17:51 +0700

    commit

--- train.py
+++ train.py
@@ -177,7 +177,8 @@ def train(train_loader, encoder, decoder, criterion, encoder_optimizer, decoder_
 
         # Remove timesteps that we didn't decode at, or are pads
         # pack_padded_sequence is an easy trick to do this
-        scores, *_ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
+        # scores, *_ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
+        scores = pack_padded_sequence(scores, decode_lengths, batch_first=True)[0]
         targets, *_ = pack_padded_sequence(targets, decode_lengths, batch_first=True)
 
         # Calculate loss
@@ -268,7 +269,8 @@ def validate(val_loader, encoder, decoder, criterion):
             # Remove timesteps that we didn't decode at, or are pads
             # pack_padded_sequence is an easy trick to do this
             scores_copy = scores.clone()
-            scores, *_ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
+            # scores, *_ = pack_padded_sequence(scores, decode_lengths, batch_first=True)
+            scores = pack_padded_sequence(scores, decode_lengths, batch_first=True)[0]
             targets, *_ = pack_padded_sequence(targets, decode_lengths, batch_first=True)
 
             # Calculate loss
