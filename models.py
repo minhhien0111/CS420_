7925f87ca5d6b5ccdb3bcaa346534d3c6c56f5b7
minhhien0111 <21520229@gm.uit.edu.vn>
2024-12-17 17:17:59 +0700

    model vgg

--- models.py
+++ models.py
@@ -5,20 +5,66 @@ import torchvision
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+# class Encoder(nn.Module):
+#     """
+#     Encoder.
+#     """
+
+#     def __init__(self, encoded_image_size=14):
+#         super(Encoder, self).__init__()
+#         self.enc_image_size = encoded_image_size
+
+#         resnet = torchvision.models.resnet101(pretrained=True)  # pretrained ImageNet ResNet-101
+
+#         # Remove linear and pool layers (since we're not doing classification)
+#         modules = list(resnet.children())[:-2]
+#         self.resnet = nn.Sequential(*modules)
+
+#         # Resize image to fixed size to allow input images of variable size
+#         self.adaptive_pool = nn.AdaptiveAvgPool2d((encoded_image_size, encoded_image_size))
+
+#         self.fine_tune()
+
+#     def forward(self, images):
+#         """
+#         Forward propagation.
+
+#         :param images: images, a tensor of dimensions (batch_size, 3, image_size, image_size)
+#         :return: encoded images
+#         """
+#         out = self.resnet(images)  # (batch_size, 2048, image_size/32, image_size/32)
+#         out = self.adaptive_pool(out)  # (batch_size, 2048, encoded_image_size, encoded_image_size)
+#         out = out.permute(0, 2, 3, 1)  # (batch_size, encoded_image_size, encoded_image_size, 2048)
+#         return out
+
+#     def fine_tune(self, fine_tune=True):
+#         """
+#         Allow or prevent the computation of gradients for convolutional blocks 2 through 4 of the encoder.
+
+#         :param fine_tune: Allow?
+#         """
+#         for p in self.resnet.parameters():
+#             p.requires_grad = False
+#         # If fine-tuning, only fine-tune convolutional blocks 2 through 4
+#         for c in list(self.resnet.children())[5:]:
+#             for p in c.parameters():
+#                 p.requires_grad = fine_tune
+
 class Encoder(nn.Module):
     """
-    Encoder.
+    Encoder using VGG16.
     """
 
     def __init__(self, encoded_image_size=14):
         super(Encoder, self).__init__()
         self.enc_image_size = encoded_image_size
 
-        resnet = torchvision.models.resnet101(pretrained=True)  # pretrained ImageNet ResNet-101
-
-        # Remove linear and pool layers (since we're not doing classification)
-        modules = list(resnet.children())[:-2]
-        self.resnet = nn.Sequential(*modules)
+        # Load pretrained VGG16 from torchvision
+        vgg = torchvision.models.vgg16(pretrained=True)  # Pretrained ImageNet VGG-16
+        
+        # Remove the fully connected layers (classifier)
+        modules = vgg.features  # Use only the convolutional layers
+        self.vgg = modules
 
         # Resize image to fixed size to allow input images of variable size
         self.adaptive_pool = nn.AdaptiveAvgPool2d((encoded_image_size, encoded_image_size))
@@ -32,21 +78,21 @@ class Encoder(nn.Module):
         :param images: images, a tensor of dimensions (batch_size, 3, image_size, image_size)
         :return: encoded images
         """
-        out = self.resnet(images)  # (batch_size, 2048, image_size/32, image_size/32)
-        out = self.adaptive_pool(out)  # (batch_size, 2048, encoded_image_size, encoded_image_size)
-        out = out.permute(0, 2, 3, 1)  # (batch_size, encoded_image_size, encoded_image_size, 2048)
+        out = self.vgg(images)  # (batch_size, 512, image_size/32, image_size/32)
+        out = self.adaptive_pool(out)  # (batch_size, 512, encoded_image_size, encoded_image_size)
+        out = out.permute(0, 2, 3, 1)  # (batch_size, encoded_image_size, encoded_image_size, 512)
         return out
 
     def fine_tune(self, fine_tune=True):
         """
-        Allow or prevent the computation of gradients for convolutional blocks 2 through 4 of the encoder.
+        Allow or prevent the computation of gradients for convolutional blocks 3 through 5 of the encoder.
 
         :param fine_tune: Allow?
         """
-        for p in self.resnet.parameters():
+        for p in self.vgg.parameters():
             p.requires_grad = False
-        # If fine-tuning, only fine-tune convolutional blocks 2 through 4
-        for c in list(self.resnet.children())[5:]:
+        # Fine-tune only convolutional blocks 3 through 5
+        for c in list(self.vgg.children())[10:]:
             for p in c.parameters():
                 p.requires_grad = fine_tune
 
@@ -80,8 +126,8 @@ class Attention(nn.Module):
         """
         att1 = self.encoder_att(encoder_out)  # (batch_size, num_pixels, attention_dim)
         att2 = self.decoder_att(decoder_hidden)  # (batch_size, attention_dim)
-        # att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
-        att = self.full_att(self.tanh(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
+        att = self.full_att(self.relu(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
+        # att = self.full_att(self.tanh(att1 + att2.unsqueeze(1))).squeeze(2)  # (batch_size, num_pixels)
         alpha = self.softmax(att)  # (batch_size, num_pixels)
         attention_weighted_encoding = (encoder_out * alpha.unsqueeze(2)).sum(dim=1)  # (batch_size, encoder_dim)
 
